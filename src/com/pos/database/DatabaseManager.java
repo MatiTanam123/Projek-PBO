9b4d2da7e770a95b46cd5373dea21cef1ca90ca7
beaverSec <chantiqia.rhadempta@gmail.com>
2026-03-27 13:31:36 +0700

    Initial structure for model, database, and service

---
+++ src/com/pos/database/DatabaseManager.java
@@ -0,0 +1,18 @@
+package com.pos.database;
+import java.sql.Connection;
+
+public class DatabaseManager {
+    private Connection conn;
+
+    public void getAllBooks() {
+        // Akan menampilkan katalog buku dari MySQL
+    }
+
+    public void insertTransaksi() {
+        // Akan menyimpan data ke tabel transaksi
+    }
+
+    public void updateStok() {
+        // Akan sinkronisasi stok ke MySQL
+    }
+}
---
+++ src/com/pos/model/Transaksi.java
@@ -0,0 +1,7 @@
+package com.pos.model;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Transaksi {
+    
+}
