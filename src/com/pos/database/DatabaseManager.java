a1a71ebd427f3343fc2f60fc99ed62265fae0c9b
beaverSec <chantiqia.rhadempta@gmail.com>
2026-04-17 20:39:45 +0700

    refactor: ganti bagian pw di databasemanager.java jadi no pw

--- src/com/pos/database/DatabaseManager.java
+++ src/com/pos/database/DatabaseManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/tokobuku";
     private static final String USER = "root";
-    private static final String PASS = "caca227"; 
+    private static final String PASS = ""; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
